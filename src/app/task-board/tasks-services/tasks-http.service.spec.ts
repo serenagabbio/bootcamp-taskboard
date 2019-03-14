19a615c6683e31935625ab2022d60a5865c7969b
Gabbio Serena <serena.gabbio@partner.italdesign.it>
2019-03-14 14:03:10 +0100

    test: HttpClientTestingModule add

--- src/app/task-board/tasks-services/tasks-http.service.spec.ts
+++ src/app/task-board/tasks-services/tasks-http.service.spec.ts
@@ -1,9 +1,12 @@
 import { HttpClient, HttpHandler } from '@angular/common/http';
-import { TestBed } from '@angular/core/testing';
+import { TestBed, fakeAsync, inject } from '@angular/core/testing';
 
 import { TasksHttpService } from './tasks-http.service';
 import { InjectionToken } from '@angular/core';
 import { provideAppConfig } from 'src/app/app-configuration';
+import {
+  HttpClientTestingModule, HttpTestingController
+} from '@angular/common/http/testing';
 
 describe('TasksHttpService', () => {
   let tasksHttpService: TasksHttpService;
@@ -12,15 +15,22 @@ describe('TasksHttpService', () => {
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      providers: [HttpClient, HttpHandler, TasksHttpService, provideAppConfig, {
-        provide: InjectionToken,
-        useValue: injectionToken
-      }]
+      providers: [
+        HttpClient,
+        HttpHandler,
+        TasksHttpService,
+        provideAppConfig,
+        {
+          provide: InjectionToken,
+          useValue: injectionToken
+        }
+      ],
+      imports: [HttpClientTestingModule]
     });
     tasksHttpService = TestBed.get(TasksHttpService);
-    tasksHttpService.create({title: 'title', text: 'text'}).subscribe(
-      value =>  existingTaskGuid = value.guid
-    );
+    tasksHttpService
+      .create({ title: 'title', text: 'text' })
+      .subscribe(value => (existingTaskGuid = value.guid));
   });
 
   it('should be created', () => {
@@ -28,23 +38,40 @@ describe('TasksHttpService', () => {
   });
 
   it('should create a new task and return a new complete task', () => {
-    tasksHttpService.create({title: 'title', text: 'text'}).subscribe(
-      value =>  expect(value).toBeTruthy()
-    );
+    inject(
+      [TasksHttpService, HttpTestingController],
+      fakeAsync((httpMock: HttpTestingController) => {
+        const mockTask = {
+          guid: 123,
+          title: 'title',
+          text: 'text',
+          writtenAt: new Date(),
+          isInProgress: false,
+          isComplete: false,
+          isFavourite: false
+        };
+        tasksHttpService
+          .create({ title: 'title', text: 'text' })
+          .subscribe(value => expect(value).toEqual(mockTask));
+    
+          const req = httpMock.expectOne(`${tasksHttpService.baseUrl}`);
+          expect(req.request.method).toBe("POST");
+          req.flush(mockTask);
+          httpMock.verify();
+      })
+    )
   });
 
   it('should return a single task if called get', () => {
-    tasksHttpService.get(existingTaskGuid).subscribe(
-      value => expect(value).toBeTruthy()
-    );
+    tasksHttpService
+      .get(existingTaskGuid)
+      .subscribe(value => expect(value).toBeTruthy());
   });
 
   it('should return all the tasks if called getAll', () => {
-    tasksHttpService.getAll().subscribe(
-      (values) => {
-        expect(values).toBeTruthy();
-      }
-    );
+    tasksHttpService.getAll().subscribe(values => {
+      expect(values).toBeTruthy();
+    });
   });
 
   it('should update a tasks', () => {
@@ -57,19 +84,15 @@ describe('TasksHttpService', () => {
       isComplete: false,
       isFavourite: false
     };
-    tasksHttpService.update(newTask).subscribe(
-      (value) => {
-        expect(value).toBeTruthy();
-      }
-    );
+    tasksHttpService.update(newTask).subscribe(value => {
+      expect(value).toBeTruthy();
+    });
   });
 
   it('should delete a task', () => {
-    tasksHttpService.delete(existingTaskGuid).subscribe(
-      (value) => {
-        expect(value).toBeTruthy();
-      }
-    );
+    tasksHttpService.delete(existingTaskGuid).subscribe(value => {
+      expect(value).toBeTruthy();
+    });
   });
 
 });
